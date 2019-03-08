6b0e5a5b22f7f812bd6b87aa0c49731e25978136
naveenperera777 <naveenperera777@gmail.com>
2019-03-08 14:48:01 +0530

    Initial commit

---
+++ src/main/java/naveen/perera/io/shipwreck/Controller/HomeController.java
@@ -0,0 +1,14 @@
+package naveen.perera.io.shipwreck.Controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @RequestMapping("/")
+    public String home(){
+        return "Test";
+    }
+
+}
